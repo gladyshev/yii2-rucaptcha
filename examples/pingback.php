7ec8b8604de9d9fd56d343cf4fdea99fb193aceb
Dmitry Gladyshev <deel@email.ru>
2017-07-27 21:45:10 +0300

    stable

--- examples/pingback.php
+++ examples/pingback.php
@@ -7,14 +7,14 @@ $rucaptcha = Yii::$app->rucaptcha;
 $pingbackUrl = 'http://' . getenv('__HOST__') .'/captcha/pingback.php';
 
 // Add pingback url to allowed list
-$client->addPingback($pingbackUrl);
+$rucaptcha->addPingback($pingbackUrl);
 
 // List of allowed pingbacks
 $allowedPingbacks = $client->getPingbacks();
 
 var_dump($allowedPingbacks);
 
-$client->deletePingback($pingbackUrl);
+$rucaptcha->deletePingback($pingbackUrl);
 
 // Check
-var_dump($client->getPingbacks());
+var_dump($rucaptcha->getPingbacks());
